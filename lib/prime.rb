937bf6b58d8ea9032900a6bb8fd7a53d3a8a0e07
Randy Leighton <randyleighton@yahoo.com>
2014-08-03 06:33:42 +0000

    add more test, complete code

--- lib/prime.rb
+++ lib/prime.rb
@@ -3,7 +3,12 @@ def prime_sifter(number)
   prime_numbers = []
   prime = 2
     
-  
+  until numbers_to_sift.length == 0 do
+    prime_numbers << prime
+    numbers_to_sift.delete_if {|num| num % prime == 0}
+    prime = numbers_to_sift[0]
+  end
+prime_numbers
 end
   
 prime_sifter(27)
--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -7,5 +7,9 @@ describe 'prime' do
     sift_values = (2..9).to_a
     expect(sift_values).to eq ([2,3,4,5,6,7,8,9])
   end
-  it 
+  it "returns an array of prime numbers up to the user number inputted" do
+    prime_sifter(27)
+    expect(prime_sifter(27)).to eq [2,3,5,7,11,13,17,19,23]
+  end
+    
 end
